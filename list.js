52bd7afeacd7b7c77c6d0e0ce3368d1464c07a7c
Bruno Michel <bruno.michel@af83.com>
2011-05-04 16:24:43 +0200

    Show only my time entries with `intervals ls`

--- list.js
+++ list.js
@@ -8,10 +8,13 @@ module.exports = function(conf, opts) {
     var start = opts.start || dateFormat(new Date(), 'yyyy-mm-dd');
     console.log("Show timesheet from %s to %s", start, end);
     var client = intervals.createClient(conf.token);
-    client.time({datebegin: start,
-                 dateend: end}, function(err, response) {
-                     showTime(response.body);
-                 });
+    client.me(function(err, res) {
+        client.time({personid: res.body.personid,
+                     datebegin: start,
+                     dateend: end}, function(err, response) {
+                         showTime(response.body);
+                     });
+    });
 
 }
 
@@ -19,7 +22,7 @@ function showTime(body) {
     // instantiate
     var table = new Table({
         head: ['Project', 'date', 'hours', 'billable']
-      , colWidths: [40, 12, 7, 10]
+      , colWidths: [42, 12, 7, 10]
     });
 
     body.time.forEach(function(time) {
